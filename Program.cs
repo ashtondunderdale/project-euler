0237f306d196d71231044b6f92a5347358beda50
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-03-22 23:43:42 +0000

    problem 7

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ internal class Program
 {
     static void Main()
     {
-        P6.Run();
+        P7.Run();
     }
 }
---
+++ problem-7.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class P7
+{
+    public static void Run()
+    {
+        var primeNum = Enumerable.Range(2, int.MaxValue - 1)
+            .Where(num => Enumerable.Range(2, (int)Math.Sqrt(num) - 1).All(i => num % i != 0))
+            .Take(10001).Last();
+
+        Console.WriteLine(primeNum);
+    }
+}
