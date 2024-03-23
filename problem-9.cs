ded86a17ef26065968e9e56f244686b8da84a30a
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-03-23 09:00:04 +0000

    problem 9

---
+++ problem-9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace proj_euler
+{
+    internal class P9
+    {
+        public static void Run()
+        {
+            int targetSum = 1000;
+
+            for (int a = 1; a < targetSum; a++)
+            {
+                for (int b = a + 1; b < targetSum; b++)
+                {
+                    int c = targetSum - a - b;
+
+                    if (a * a + b * b == c * c)
+                    {
+                        long product = (long)a * b * c;
+                        Console.WriteLine(product);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
