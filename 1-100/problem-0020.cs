ef255d80351abc0ed75cd5c9fa4680b4bb0e65cb
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-03-25 16:33:47 +0000

    problem 20

--- 1-100/problem-0020.cs
+++ 1-100/problem-0020.cs
@@ -6,6 +6,16 @@ internal class P20
 {
     public static void Run() 
     {
+        BigInteger factorialSum = 1;
 
+        for (int i = 100; i > 0; i--)
+            factorialSum *= i;
+        
+        int digitSum = 0;
+
+        foreach (char c in factorialSum.ToString())
+            digitSum += c - '0';
+        
+        Console.Write(digitSum);
     }
 }
