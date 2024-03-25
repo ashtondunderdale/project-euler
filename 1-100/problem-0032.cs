d80fb2cc97231bfc57c4c999e00ac8dafda62def
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-03-25 21:25:24 +0000

    problem 32

---
+++ 1-100/problem-0032.cs
@@ -0,0 +1,35 @@
+﻿namespace proj_euler;
+
+internal class P32
+{
+    public static void Run()
+    {
+        List<int> productSum = new();
+
+        for (int i = 1; i < 10000; i++)
+        {
+            for (int j = 1; j < 10000; j++)
+            {
+                int product = i * j;
+                string concatenatedString = product.ToString() + i.ToString() + j.ToString();
+
+                if (IsPandigital(concatenatedString) && !productSum.Contains(product))
+                    productSum.Add(product);
+            }
+        }
+
+        Console.Write(productSum.Sum());
+    }
+
+    private static bool IsPandigital(string concatenatedString)
+    {
+        if (concatenatedString.Length != 9)
+            return false;
+
+        for (int digit = 1; digit <= 9; digit++)
+            if (!concatenatedString.Contains(digit.ToString()))
+                return false;
+        
+        return true;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ internal class ProjectEuler
 {
     static void Main()
     {
-        P31.Run();
+        P32.Run();
     }
 }
