055b9051a9d3eb9441fac083bf30ea881a4f3ac1
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-03-25 18:52:43 +0000

    problem 25

---
+++ 1-100/problem-0025.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace proj_euler;
+
+internal class P25
+{
+    public static void Run() 
+    {
+        BigInteger first = 0, second = 1;
+        int index = 0;
+
+        while (first.ToString().Length < 1000)
+        {
+            index++;
+
+            BigInteger newTerm = first + second;
+            first = second;
+            second = newTerm;
+        }
+
+        Console.WriteLine(index);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ internal class ProjectEuler
 {
     static void Main()
     {
-        P22.Run();
+        P25.Run();
     }
 }
