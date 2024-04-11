cb3c75e5a3705e5c3666d3d254865f1e87261173
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-04-11 13:19:58 +0100

    problem 48

---
+++ 1-100/problem-0048.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace proj_euler;
+
+internal class P48
+{
+    public static void Run()
+    {
+        BigInteger sum = 0;
+
+        for (int i = 1; i <= 1000; i++)
+            sum += Power(i, i);
+
+        Console.Write(sum.ToString()[^10..]);
+    }
+
+    public static BigInteger Power(int x, int y) => BigInteger.Pow(x, y);
+}
--- Program.cs
+++ Program.cs
@@ -2,5 +2,5 @@
 
 internal class ProjectEuler
 {
-    static void Main() => P47.Run();
+    static void Main() => P48.Run();
 }
